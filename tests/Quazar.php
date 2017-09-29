64f47aab6668f02d2267ad05df08a78494d2bc98
e200 <eleandro@inbox.ru>
2017-09-30 00:35:10 +0100

    Added @author PHPDoc

--- tests/Quazar.php
+++ tests/Quazar.php
@@ -2,6 +2,11 @@
 
 namespace Tests;
 
+/**
+ * Class MakeAccessibleTest.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ */
 class Quazar
 {
     protected $alphaCentaurus = '4.37 light/year';
