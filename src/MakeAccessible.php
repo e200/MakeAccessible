ded8638a51d79f9f76e216a5e31d17706f7860ff
e200 <eleandro@inbox.ru>
2017-09-29 21:51:55 +0100

    Added exceptions, PHPDocs, and renamed functions variables.

--- src/MakeAccessible.php
+++ src/MakeAccessible.php
@@ -3,72 +3,174 @@
 namespace e200\MakeAccessible;
 
 use ReflectionClass;
+use e200\MakeAccessible\Exceptions\InvalidObjectInstanceException;
+use e200\MakeAccessible\Exceptions\MethodNotFoundException;
+use e200\MakeAccessible\Exceptions\PropertyNotFoundNotFoundException;
 
+/**
+ * Class MakeAccessible.
+ *
+ * Makes encapsulated instances accessible.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ */
 class MakeAccessible
 {
+    /** @var object */
     protected $instance;
+
+    /** @var \ReflectionClass */
     protected $refClass;
 
-    function __construct($instance)
+    /*
+     * MakeAccessible constructor.
+     *
+     * @param object $instance. The instance that will be reflected.
+     */
+    protected function __construct($instance)
+    {
+        $refClass = new ReflectionClass($instance);
+
+        $this->setInstance($instance);
+        $this->setRefClass($refClass);
+    }
+
+    protected function __clone() {}
+
+    /**
+     * @param object $instance The instance that you'll gain access.
+     *
+     * @return MakeAccessible
+     *
+     * @throws InvalidObjectInstanceException
+     */
+    static function make($instance)
     {
         if (is_object($instance)) {
-            $this->instance = $instance;
-
-            $this->refClass = new ReflectionClass($instance);
+            return new MakeAccessible($instance);
         } else {
-            throw new InvalidObjectIntanceException("Invalid object instance provided.");
+            throw new InvalidObjectInstanceException("Invalid instance provided.");
         }
     }
 
     /**
-     * Calls `$name` method in the `MakeAccessible::$instance`.
+     * Sets the `MakeAccessible::$instance`.
      *
-     * @param string $name     Method name.
+     * @param object $instance
+     */
+    protected function setInstance($instance)
+    {
+        $this->instance = $instance;
+    }
+
+    /**
+     * Gets the `MakeAccessible::$instance`.
+     *
+     * @return object
+     */
+    protected function getInstance()
+    {
+        return $this->instance;
+    }
+
+    /**
+     * Sets the `MakeAccessible::$refClass`.
+     *
+     * @param \ReflectionClass $refClass
+     */
+    protected function setRefClass($refClass)
+    {
+        $this->refClass = $refClass;
+    }
+
+    /**
+     * Gets the `MakeAccessible::$refClass`.
+     *
+     * @return \ReflectionClass
+     */
+    protected function getRefClass()
+    {
+        return $this->refClass;
+    }
+
+    /**
+     * Calls `$propertyName` method in the `MakeAccessible::$instance`.
+     *
+     * @param string $methodName Method name.
      * @param array $arguments Method arguments.
      *
      * @return mixed
+     *
+     * @throws MethodNotFoundException
      */
-    function __call($name, $arguments)
+    function __call($methodName, $arguments)
     {
-        if ($this->reflectedClass->hasMethod($name)) {
-            $method = $this->reflectedClass->getMethod($name);
+        $refClass = $this->getRefClass();
+
+        if ($refClass->hasMethod($methodName)) {
+            $method = $refClass->getMethod($methodName);
 
             $this->unlock($method);
 
-            return $method->invokeArgs($this->instance, $arguments);
+            return $method->invokeArgs($this->getInstance(), $arguments);
         } else {
-            throw new MethodNotFoundException("No method {$name} was found in the provided instance.");
+            throw new MethodNotFoundException("No method \"{$methodName}\" was found on class \"{$refClass->getName()}\".");
         }
     }
 
     /**
-     * Access `$name` property in the `MakeAccessible::$instance`.
+     * Gets the `$propertyName`.
      *
-     * @param string $name Property name.
+     * @param string $propertyName Property name.
      *
      * @return mixed
+     *
+     * @throws PropertyNotFoundNotFoundException
      */
-    function __get($name)
+    function __get($propertyName)
     {
-        if ($this->reflectedClass->hasProperty($name)) {
-            $property = $this->reflectedClass->getProperty($name);
+        $refClass = $this->getRefClass();
+
+        if ($refClass->hasProperty($propertyName)) {
+            $property = $refClass->getProperty($propertyName);
 
             $this->unlock($property);
 
-            return $property->getValue($this->instance);
+            return $property->getValue($this->getInstance());
         } else {
-            throw new PropertyNotFoundNotFoundException("No property {$name} was found in the provided instance.");
+            throw new PropertyNotFoundNotFoundException("No property \"{$propertyName}\" was found on class \"{$refClass->getName()}\".");
         }
     }
 
     /**
-     * Unlocks if encapsuled a property or a method inside `MakeAccessible::$instance`.
+     * Access `$propertyName` and insert the `$value` into it.
+     *
+     * @param string $propertyName.
+     * @param mixed  $value
+     *
+     * @throws PropertyNotFoundNotFoundException
+     */
+    function __set($propertyName, $value)
+    {
+        $refClass = $this->getRefClass();
+
+        if ($refClass->hasProperty($propertyName)) {
+            $property = $refClass->getProperty($propertyName);
+
+            $this->unlock($property);
+
+            $property->setValue($this->getInstance(), $value);
+        } else {
+            throw new PropertyNotFoundNotFoundException("No property {$propertyName} was found on class \"{$refClass->getName()}\".");
+        }
+    }
+
+    /**
+     * Unlocks if encapsulated a property or a method inside `MakeAccessible::$instance`.
      *
      * @param \ReflectionMethod|\ReflectionProperty $refObject
-     *
-     * @return mixed
      */
-    function unlock($refObject)
+    protected function unlock($refObject)
     {
         if ($refObject->isProtected() || $refObject->isPrivate()) {
             $refObject->setAccessible(true);
