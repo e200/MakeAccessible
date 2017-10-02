6addeb4b799da75a2e1211d6aed1d84ca34d0b21
e200 <eleandro@inbox.ru>
2017-10-02 01:03:29 +0100

    Removed instantiateSingleton function

--- src/Reflector.php
+++ src/Reflector.php
@@ -2,7 +2,6 @@
 
 namespace e200\MakeAccessible;
 
-use e200\MakeAccessible\Exceptions\NonSingletonClassException;
 use ReflectionClass;
 
 /**
@@ -12,37 +11,14 @@ use ReflectionClass;
   */
  class Reflector
  {
-     /** @var \ReflectionClass */
-     protected $refClass;
-
-     /**
-      * Reflector constructor.
-      *
-      * @param object|string The class name.
-      */
-     public function __construct($class)
-     {
-         $this->refClass = $this->reflectClass($class);
-     }
-
      /**
       * Makes a new ReflectionClass instance.
       *
       * @return ReflectionClass
       */
-     public function reflectClass($class)
+     public static function reflect($class)
      {
          return new ReflectionClass($class);
-     }
-
-     /**
-      * Gets the reflected class instance.
-      *
-      * @return ReflectionClass
-      */
-     public function getReflectedClass()
-     {
-         return $this->refClass;
      }
 
      /**
@@ -77,52 +53,5 @@ use ReflectionClass;
      public function makeAccessible($refObject)
      {
          $refObject->setAccessible(true);
-     }
-
-     /**
-      * @param string $singletonClass
-      * @param array $arguments       Constructor arguments.
-      *
-      * @throws NonSingletonClassException
-      *
-      * @return object
-      */
-     public function instantiateSingleton($singletonClass, $arguments = [])
-     {
-         // Getting the reflection class that represents the `$singletonClass`.
-         $refClass = $this->reflectClass($singletonClass);
-
-         // Getting the constructor of the `$refClass`.
-         $constructor = $refClass->getConstructor();
-
-         /*
-          * Every singleton class has a protected or private constructor.
-          *
-          * If our class constructor is null, that means our class hasn't a
-          * constructor, what also means that our class isn't a singleton class.
-          *
-          * In this case we throw a `NonSingletonClassException`.
-          */
-         if (is_null($constructor)) {
-             throw new NonSingletonClassException('Trying to instantiate a non singleton class.');
-         } else {
-             /*
-              * Here, we're instantiating our class without invoke the constructor.
-              *
-              * Since we have the class instance, we can now reflect this instance,
-              * make the constructor accessible and then invoke it, we're making
-              * the exactly same process that php makes to instantiate a normal
-              * class, but we're doing it with a singleton class, amazing uh???
-              */
-             $instance = $refClass->newInstanceWithoutConstructor();
-
-             // First we make accessible our constructor (since is protected or private)
-             $this->makeAccessible($constructor);
-
-             // Next put `$arguments` into the constructor and then we invoke it.
-             $constructor->invokeArgs($instance, $arguments);
-
-             return $instance;
-         }
      }
  }
